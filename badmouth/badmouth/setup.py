e2b7bcc91667ce3b52dcca0b281c234f3d48b490
Aashish Tripathee <helloworld@aashishtripathee.com>
2014-10-04 20:58:41 -0400

    Pyramid

---
+++ badmouth/badmouth/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='badmouth',
+      version='0.0',
+      description='badmouth',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='badmouth',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = badmouth:main
+      [console_scripts]
+      initialize_badmouth_db = badmouth.scripts.initializedb:main
+      """,
+      )
