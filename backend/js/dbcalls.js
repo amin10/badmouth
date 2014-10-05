c777c3a3cfead19f40a1f1020518c0984d30df93
Prafulla <prafulla@mit.edu>
2014-10-05 01:48:03 -0400

    some functions

--- backend/js/dbcalls.js
+++ backend/js/dbcalls.js
@@ -1,18 +1,38 @@
 Parse.initialize("s32blhuMK0fzjARmluinIoP7GMNCsWXPsREdsFhD", "geSjnD0tu528nZjZHMKPCQ9XB9EbGKCoaUGdXf56");
 
+var Target = Parse.Object.extend("Target");
+var Badmouth = Parse.Object.extend("Badmouth");
 
-
-function get_targets(){
-    var Target = Parse.Object.extend("Target");
+function get_target_promise(){
     var query = new Parse.Query(Target);
     var d = new Date();
     var time = (5 * 24 * 3600 * 1000);
     var expirationDate = new Date(d.getTime() - (time));
     query.greaterThan("createdAt",  expirationDate);
     query.descending("bmcount");
-    query.find().then(function(results) {
-     return results;
-    })
+    return query.find();
 }
 
+function populate_table(){
+promise = get_target_promise();
+promise.then(function(results) {
+  console.log(results);  
+})
+}
+                        
+function get_badmouths(target_id) {
+    var query = new Parse.Query(Target);
+    query.equalTo("objectId",target_id);
+    return query.find();
+}
+
+function init_badmouths(){
+promise = get_target_promise();
+promise.then(function(results) {
+    for (i = 0; i < results.length; i++) {
+        results[i].relation("badmouths").query().find().then(function (results2) {
+            console.log(results2[0].get("text")); }); 
+    } 
+})
+}
 
