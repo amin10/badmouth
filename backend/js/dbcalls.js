92c7f6faee0d31c9f2ae1d2185d8e587353929da
BryanCai <bryan.x.cai@gmail.com>
2014-10-05 01:49:30 -0400

    implemented insert_badmouth

--- backend/js/dbcalls.js
+++ backend/js/dbcalls.js
@@ -2,17 +2,44 @@ Parse.initialize("s32blhuMK0fzjARmluinIoP7GMNCsWXPsREdsFhD", "geSjnD0tu528nZjZHM
 
 
 
-function get_targets(){
-    var Target = Parse.Object.extend("Target");
-    var query = new Parse.Query(Target);
-    var d = new Date();
-    var time = (5 * 24 * 3600 * 1000);
-    var expirationDate = new Date(d.getTime() - (time));
-    query.greaterThan("createdAt",  expirationDate);
-    query.descending("bmcount");
-    query.find().then(function(results) {
-     return results;
-    })
+function insert_badmouth(target_id, badmouth_text){
+	var Target = Parse.Object.extend("Target");
+	var target = new Target();
+	target.id = target_id;	
+
+	var Badmouth = Parse.Object.extend("Badmouth");
+	var badmouth = new Badmouth();
+	badmouth.set("text", badmouth_text);
+	badmouth.set("parent", target);
+
+	badmouth.save(null, {
+		success: function(badmouth) {
+    		alert('New object created with objectId: ' + badmouth.id);
+		},
+		error: function(badmouth, error) {
+    		alert('Failed to create new object, with error code: ' + error.message);
+		}
+	});
+
+	target.fetch({
+    	success: function(target) {
+    		target.increment("bmcount");
+    		var relation = target.relation("badmouths");
+    		relation.add(badmouth);
+    		target.save(null, {
+				success: function(target) {
+    				alert('New object created with objectId: ' + target.id);
+				},
+				error: function(target, error) {
+    				alert('Failed to create new object, with error code: ' + error.message);
+				}
+
+			});
+    		console.log("here");
+		},
+		error: function(target, error) {
+			console.log(error);
+		}
+	});
+
 }
-
-
